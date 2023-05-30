6b4d807a0a49ddba5200f0eb839da3db26913b13
Maxim Epishev <epishev.m@mail.ru>
2023-05-31 00:46:25 +0300

    Add CurrencyTooltipContextExtensions

---
+++ Runtime/Extensions/CurrencyTooltipContextExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace EM.GameKit.Context
+{
+
+using Foundation;
+
+public static class CurrencyTooltipContextExtensions
+{
+	public static Context BindCurrencyTooltip(this Context context)
+	{
+		var lifeTime = context.IsGlobalContext
+			? LifeTime.Global
+			: LifeTime.Local;
+
+		context.DiContainer
+			.Bind<CurrencyTooltipRouter>()
+			.SetLifeTime(lifeTime)
+			.To<CurrencyTooltipRouter>()
+			.ToSingleton();
+
+		context.DiContainer
+			.Bind<SimpleCurrencyTooltipViewModel>()
+			.SetLifeTime(lifeTime)
+			.To<SimpleCurrencyTooltipViewModel>();
+
+		return context;
+	}
+}
+
+}
